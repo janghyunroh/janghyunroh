8f6412f6ce2042d656d7babd12d87afbef2a228c
janghyunroh <janghyunroh@gmail.com>
2022-12-17 16:39:11 +0900

    first commit

---
+++ TicTacToe.py
@@ -0,0 +1,15 @@
+import random
+
+def drawboard(board) :
+  print('   |   |')
+  print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9]) 
+  print('   |   |')
+  print('-----------')
+  print('   |   |')
+  print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9]) 
+  print('   |   |')
+  print('-----------')
+  print('   |   |')
+  print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9]) 
+  print('   |   |')
+  
